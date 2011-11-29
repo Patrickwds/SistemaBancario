ce4ceb010fa6b16fd257fbe1e5c7d14c8f2fca89
patrickwds <patrickwds1@gmail.com>
2011-11-28 20:34:12 -0330

    Sistema bancario

---
+++ src/ContaCorrente.java
@@ -0,0 +1,12 @@
+
+public class ContaCorrente {
+	
+	String nomeDoCliente;
+	String numeroDaConta;
+	String numeroDaAgencia;
+	double saldo;
+	
+	public void sacar(double valor){
+		
+	}
+}
---
+++ src/ProgramaPrincipal.java
@@ -0,0 +1,71 @@
+import java.util.Scanner;
+import java.util.Vector;
+
+public class ProgramaPrincipal {
+
+	private static final String CONTINUAR = "S";
+	
+	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+			
+		System.out.println("----------------------");
+		
+		String nomeDoCliente;
+		String numeroAgencia;
+		String numeroConta;
+		double SaldoDigitado;
+		ContaCorrente contaDoCliente;
+		String continuar = "S";
+		
+		Vector<ContaCorrente> NovaConta = new Vector<ContaCorrente>();
+		
+		while(CONTINUAR.equalsIgnoreCase(continuar)){
+			
+				System.out.println("Digite o nome:");
+				nomeDoCliente = entrada.nextLine();
+				System.out.println("Digite numero de agencia:");
+				numeroAgencia = entrada.nextLine();
+				System.out.println("Digite numero de conta:");
+				numeroConta = entrada.nextLine();
+				System.out.println("Digite o saldo atual:");
+				SaldoDigitado = Double.parseDouble(entrada.nextLine());
+			
+			
+				contaDoCliente = new ContaCorrente();
+				
+				contaDoCliente.nomeDoCliente = new String(nomeDoCliente);
+				contaDoCliente.numeroDaAgencia = (numeroAgencia);
+				contaDoCliente.numeroDaConta = (numeroConta);
+				contaDoCliente.saldo = (SaldoDigitado);
+	
+				NovaConta.add(contaDoCliente);
+
+				for(int i= 0; i< NovaConta.size(); i++){
+					ContaCorrente conta = NovaConta.get(i);
+					System.out.println("Nome do Usuario: " +conta.nomeDoCliente);
+					System.out.println("Nome do Usuario: " +conta.numeroDaAgencia);
+					System.out.println("Nome do Usuario: " +conta.numeroDaConta);
+					System.out.println("Nome do Usuario: " +conta.saldo);
+				}				
+				
+				System.out.println("continuar");
+				continuar = entrada.nextLine();		
+				
+
+			}	
+
+
+			//ContaCorrente contaDaMaria;
+			//contaDaMaria = new ContaCorrente();
+
+			//contaDaMaria.nome = new String("Maria da Silva");
+			//contaDaMaria.numeroDaAgencia = "4858-X";
+			//contaDaMaria.numeroDaConta = "26976-5";
+			//contaDaMaria.saldo = 200;
+
+			//System.out.println("Nome do cliente: "+ contaDaMaria.nome);
+			//System.out.println("Saldo do cliente: "+ contaDaMaria.saldo);
+
+	}
+
+}
